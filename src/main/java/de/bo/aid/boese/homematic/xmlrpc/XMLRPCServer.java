588dba9416328b3757a09e2cebb6ddba996ff1a1
sebastian <sebastian.lechte@hs-bochum.de>
2015-11-22 22:51:53 +0100

    fix for sonarcube
    Change-Id: I4c4be91f7678ee4e221a647934a480e911b18092

--- src/main/java/de/bo/aid/boese/homematic/xmlrpc/XMLRPCServer.java
+++ src/main/java/de/bo/aid/boese/homematic/xmlrpc/XMLRPCServer.java
@@ -38,7 +38,6 @@ import java.io.IOException;
 
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.LogManager;
-import org.apache.xmlrpc.XmlRpcException;
 import org.apache.xmlrpc.server.PropertyHandlerMapping;
 import org.apache.xmlrpc.server.XmlRpcServer;
 import org.apache.xmlrpc.server.XmlRpcServerConfigImpl;
