6eeac2c5f5d5d0d736fdf2106d1187fcd9a9bd67
Robin <Robinpeuker@web.de>
2016-01-26 11:02:25 +0100

    BugFix XML-RPC handler
    Change-Id: I5d90be13514986e01e72a5e98c1296db47566b24

--- src/main/java/de/bo/aid/boese/homematic/xmlrpc/XMLRPCServer.java
+++ src/main/java/de/bo/aid/boese/homematic/xmlrpc/XMLRPCServer.java
@@ -38,6 +38,7 @@ import java.io.IOException;
 
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.LogManager;
+import org.apache.xmlrpc.XmlRpcException;
 import org.apache.xmlrpc.server.PropertyHandlerMapping;
 import org.apache.xmlrpc.server.XmlRpcServer;
 import org.apache.xmlrpc.server.XmlRpcServerConfigImpl;
@@ -66,9 +67,22 @@ public class XMLRPCServer {
 	          XmlRpcServer xmlRpcServer = webServer.getXmlRpcServer();
 	        
 	          PropertyHandlerMapping phm = new PropertyHandlerMapping();
-	          XMLRPCSystemHandler handler = new XMLRPCSystemHandler();
-	          handler.addHandler(phm);
-
+//	          XMLRPCMessageHandler handler = new XMLRPCMessageHandler();
+	          //XMLRPCSystemHandler handler = new XMLRPCSystemHandler();
+	          //handler.addHandler(phm);
+	          
+	          
+	          try {
+	       	                phm.load(Thread.currentThread().getContextClassLoader(),
+	       	                           "MyHandlers.properties");
+	       	            } catch (IOException e) {
+	       	                // TODO Auto-generated catch block
+	       	                e.printStackTrace();
+	       	            } catch (XmlRpcException e) {
+	       	                // TODO Auto-generated catch block
+	       	                e.printStackTrace();
+	       	            }
+	          
 	          xmlRpcServer.setHandlerMapping(phm);
 	        
 	          XmlRpcServerConfigImpl serverConfig =
